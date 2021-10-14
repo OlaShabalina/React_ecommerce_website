27992e5a3034b6a2733984b2666fa51b607718a8
OlaShabalina <o.shabalina@outlook.com>
2021-10-14 22:19:18 +1100

    created a homepage element (approx layout)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Homepage from './components//homepage/Homepage'
 
 function App() {
   return (
     <div>
-      Hello!
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,38 @@
+export default function Homepage() {
+  return (
+    <div className='homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>JACKETS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>BAGS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>WOMEN</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>MEN</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
