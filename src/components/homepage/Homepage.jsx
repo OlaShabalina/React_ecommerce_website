1e1eb46c79a8feccc6cb080d96f1d077c187dd22
OlaShabalina <o.shabalina@outlook.com>
2021-10-14 22:43:49 +1100

    used sass to create a responsive alignment for the homepage

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -1,34 +1,36 @@
+import './Homepage.style.scss'
+
 export default function Homepage() {
   return (
     <div className='homepage'>
       <div className='directory-menu'>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>HATS</h1>
+            <h1 className='title'>Summer</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>JACKETS</h1>
+            <h1 className='title'>Jumpsuits</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>BAGS</h1>
+            <h1 className='title'>Tops</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>WOMEN</h1>
+            <h1 className='title'>Dresses</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
         <div className='menu-item'>
           <div className='content'>
-            <h1 className='title'>MEN</h1>
+            <h1 className='title'>Knitwear</h1>
             <span className='subtitle'>SHOP NOW</span>
           </div>
         </div>
