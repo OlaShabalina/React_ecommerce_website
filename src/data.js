2f3b2029f4d0452d3e1ff797cf1668be954997a7
OlaShabalina <o.shabalina@outlook.com>
2021-10-17 21:00:28 +1100

    populated items array with approx products for summer sections

--- src/data.js
+++ src/data.js
@@ -4,7 +4,63 @@ export const  shopSections = [
     id: 1,
     title: 'Summer',
     img: 'https://www.alux.com/wp-content/uploads/2014/04/Summer-Dresses-Fashion-Trends-2014-Model-Lace-3.jpg',
-    linkUrl: 'summer'
+    linkUrl: 'summer',
+    items: [
+      {
+        id: 1,
+        name: 'Que Dress',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/114d8771-ab8e-4b6b-9307-3b8656340015_360x.jpg?v=1634169121',
+        price: 108
+      },
+      {
+        id: 2,
+        name: 'Micro Beaded Bag',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/fbacaa83-34c8-4879-b2c3-805be7f46d92_360x.jpg?v=1630883639',
+        price: 68
+      },
+      {
+        id: 3,
+        name: 'Sundae Shorts',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/12245ee0-d75a-40e2-b539-59250ce2957e_360x.jpg?v=1634167116',
+        price: 58
+      },
+      {
+        id: 4,
+        name: 'Sundae Top',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/bed42683-8f24-4dd8-a9f5-0bbf862f786b_360x.jpg?v=1634169138',
+        price: 78
+      },
+      {
+        id: 5,
+        name: 'Moko Off Shoulder Dress',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/72deba11-6fd8-4d65-aa15-6171a5e8a56a_360x.jpg?v=1634168813',
+        price: 92
+      },
+      {
+        id: 6,
+        name: 'Anica Off Shoulder Top',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/86c130df-ad90-457d-992a-fe8290c036bc_360x.jpg?v=1633666733',
+        price: 62
+      },
+      {
+        id: 7,
+        name: 'Anica Shorts',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/ea873d34-5ed1-4d20-8b63-31c4cd7669ec_360x.jpg?v=1633666632',
+        price: 62
+      },
+      {
+        id: 8,
+        name: 'Bobbi Shirred Dress',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/d64f97fd-8e40-45fd-afc0-6709af61a1fc_360x.jpg?v=1633925651',
+        price: 98
+      },
+      {
+        id: 9,
+        name: 'Lista Sunglasses - Tortoise',
+        imageUrl: 'https://cdn.shopify.com/s/files/1/0108/8959/2898/products/9d3806eb-61ba-4fb8-85ba-0722b27a1d24_360x.jpg?v=1633671566',
+        price: 48
+      }
+    ]
   },
   {
     id: 2,
